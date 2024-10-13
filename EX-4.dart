346b2721aa00b9bb63e18b1e8f4ef9475c907782
nith <hengpanharanith099@gmail.com>
2024-10-13 22:13:39 +0700

    Nith

--- EX-4.dart
+++ EX-4.dart
@@ -1,16 +1,20 @@
 void main() {
-  // Map of pizza prices
+  // Initialize total to 0
   double total = 0;
+
   const pizzaPrices = {
     'margherita': 5.5,
     'pepperoni': 7.5,
     'vegetarian': 6.5,
   };
 
-  // Example order
   const order = ['margherita', 'pepperoni', 'pineapple'];
+
   for (var food in order) {
-    total += pizzaPrices[food];
+    if (pizzaPrices.containsKey(food)) {
+      total += pizzaPrices[food]!;
+    } else {
+      print('$food is not available.');
+    }
   }
-  // Your code
 }
