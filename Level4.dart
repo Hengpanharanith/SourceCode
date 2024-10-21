1910abb974e60b5152c2d8985df8d969067ba406
nith <hengpanharanith099@gmail.com>
2024-10-21 09:36:00 +0700

    nith

--- Level4.dart
+++ Level4.dart
@@ -1,44 +1,31 @@
 class Distances {
-  double? cms;
   double? meters;
-  double? kms;
-  double? get cm {
-    return cms;
-  }
 
   double? get meter {
     return meters;
-    
-  }
-
-  double? get km {
-    return kms;
   }
 
   Distances.kms(double? kms) {
     if (kms != null) {
-      this.kms = kms;
-      this.cms = kms * 100000;
       this.meters = kms * 1000;
     }
   }
+
   Distances.meters(double? meters) {
     if (meters != null) {
       this.meters = meters;
-      this.cms = meters * 100;
-      this.kms = meters / 1000;
     }
   }
+
   Distances.cms(double? cms) {
     if (cms != null) {
-      this.cms = cms;
       this.meters = cms / 100;
-      this.kms = cms / 10000;
     }
   }
+
   @override
   String toString() {
-    return 'Distances(kms: $kms, meters: $meters, cms:$cms)';
+    return 'Distances(meters: $meters)';
   }
 
   Distances operator +(Distances other) {
